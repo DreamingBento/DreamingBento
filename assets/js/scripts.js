b3ea0c0468a19237fa3616c796e7ea1a9c48e69e
Michael Mangrobang <micky.jm4@gmail.com>
2017-12-13 01:05:24 -1000

    updated game page! Now has the website's format!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,51 @@
-$(
-    function menuDropdown() {
-    $('.dropdown')
-    .dropdown({
-        // you can use any ui transition
-        transition: 'drop'
-    });
-    }
-);
+// Used for the animations
+document.addEventListener('DOMContentLoaded', function () {
+
+        $('#quote-row')
+            .transition('hide')
+            .transition('fade right', '2s')
+        ;
+
+        $('#playto-banner')
+            .transition('hide')
+            .transition('fly down', '1s')
+            .transition('tada', '1s')
+        ;
+
+});
+
+/** Modal Functions **/
+function neilModal() {
+    $('#neilModal')
+    .modal('setting', 'transition', 'vertical flip')
+    .modal('show')
+    ;
+}
+
+function kaylaModal() {
+    $('#kaylaModal')
+    .modal('setting', 'transition', 'vertical flip')
+    .modal('show')
+    ;
+}
+
+function justinModal() {
+    $('#justinModal')
+    .modal('setting', 'transition', 'vertical flip')
+    .modal('show')
+    ;
+}
+
+function kaeliModal() {
+    $('#kaeliModal')
+    .modal('setting', 'transition', 'vertical flip')
+    .modal('show')
+    ;
+}
+
+function mickeyModal() {
+    $('#mickeyModal')
+    .modal('setting', 'transition', 'vertical flip')
+    .modal('show')
+    ;
+}
