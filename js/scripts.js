cd147d6c14a2918ecf8a76b9f921915dd0adb974
Michael Mangrobang <micky.jm4@gmail.com>
2018-06-04 09:51:06 -1000

    Small cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,60 @@
 $(document).ready(function () {
 
-    $('.ui.dropdown')
-        .dropdown()
-    ;
+    $('.ui.dropdown').dropdown();
 
     $('#quote-row')
         .transition('hide')
-        .transition('fade right', '2s')
-    ;
+        .transition('fade right', '2s');
 
-    $('#playto-banner').transition('hide')
+    $('#playto-banner')
+        .transition('hide')
         .transition('fly down', '1s')
-        .transition('tada', '1s')
-    ;
+        .transition('tada', '1s');
 
-    $('#playtoImg')
-        .hover(function() {
-            $(this)
-                .transition('jiggle')
-            ;
+    $('#playtoImg').hover(function() {
+            $(this).transition('jiggle');
         }, function() { })
     ;
 
-    $('#landingImg')
-        .hover(function() {
-            $(this)
-                .transition('jiggle')
-            ;
-        }, function(){ })
-    ;
+    $('#landingImg').hover(function() {
+            $(this).transition('jiggle');
+        }, function(){ });
 
     $('#neil').click(function() {
         $('#neilModal')
-        .modal('setting', 'transition', 'vertical flip')
-        .modal('show')
+            .modal('setting', 'transition', 'vertical flip')
+            .modal('show')
     });
 
     $('#kayla').click(function() {
         $('#kaylaModal')
-        .modal('setting', 'transition', 'vertical flip')
-        .modal('show')
+            .modal('setting', 'transition', 'vertical flip')
+            .modal('show')
     });
 
     $('#kaeli').click(function() {
         $('#kaeliModal')
-        .modal('setting', 'transition', 'vertical flip')
-        .modal('show')
+            .modal('setting', 'transition', 'vertical flip')
+            .modal('show')
     });
 
     $('#justin').click(function() {
         $('#justinModal')
-        .modal('setting', 'transition', 'vertical flip')
-        .modal('show')
+            .modal('setting', 'transition', 'vertical flip')
+            .modal('show')
     });
 
     $('#mickey').click(function() {
         $('#mickeyModal')
-        .modal('setting', 'transition', 'vertical flip')
-        .modal('show')
+            .modal('setting', 'transition', 'vertical flip')
+            .modal('show')
     });
 
     $('#playtoImg').click(function() {
         $('#videoModal')
-        .modal('setting', 'closable', false)
-        .modal('setting', 'transition', 'vertical flip')
-        .modal('show')
+            .modal('setting', 'closable', false)
+            .modal('setting', 'transition', 'vertical flip')
+            .modal('show')
     });
 
     $('#stopVideo').click(function() {
